e16805a61d91f0db57f70bef8dd8d9e3489a12b4
mingddo <godhsb@naver.com>
2025-01-02 16:56:17 +0900

    Update : root URL 변경

--- src/middleware.js
+++ src/middleware.js
@@ -1,6 +1,11 @@
 import { NextResponse } from "next/server";
 
-export function middleware(req) {
+export async function middleware(req) {
     const { pathname } = new URL(req.url);
     console.log(pathname);
+
+    // root -> Root Page를 home으로 설정
+    if(pathname === '/'){
+        return NextResponse.redirect(new URL('/home', req.url));
+    }
 }
