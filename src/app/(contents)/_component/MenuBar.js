639d3da5d1e08bc68b0dcd9221d884146cac396b
mingddo <godhsb@naver.com>
2025-01-16 16:31:17 +0900

    Update : 메뉴 링크 추가

--- src/app/(contents)/_component/MenuBar.js
+++ src/app/(contents)/_component/MenuBar.js
@@ -1,13 +1,17 @@
-import { Grid2 as Grid } from "@mui/material";
+import { Grid2 as Grid, Link } from "@mui/material";
 
 export default function MenuBar() {
     return (
-        <Grid container direction={"row"} spacing={"1px"} sx={{height : "100%"}}>
-            <Grid className={"text-white text-xl text-bold"} size={2} sx={{ minWidth : "130px", display : "flex",  justifyContent : "center", alignItems : "center", backgroundColor: "blue", marginLeft :"50px" }}>
-                <h1>쉬는 시간</h1>
+        <Grid container direction={"row"} spacing={"1px"} sx={{ height: "100%" }}>
+            <Grid className={"text-white text-xl text-bold"} size={2} sx={{ minWidth: "130px", display: "flex", justifyContent: "center", alignItems: "center", backgroundColor: "blue", marginLeft: "50px" }}>
+                <Link href="/game" className={["text-white", "link_no_underline"].join(" ")}>
+                    <h1>쉬는 시간</h1>
+                </Link>
             </Grid>
-            <Grid className={"text-white text-xl text-bold"} size={2} sx={{ minWidth : "130px", display : "flex", justifyContent : "center", alignItems : "center", backgroundColor: "blue", marginLeft :"50px" }}>
-                <h3>행운 시험</h3>
+            <Grid className={"text-white text-xl text-bold"} size={2} sx={{ minWidth: "130px", display: "flex", justifyContent: "center", alignItems: "center", backgroundColor: "blue", marginLeft: "50px" }}>
+                <Link href="/game" className={["text-white", "link_no_underline"].join(" ")}>
+                    <h3>행운 시험</h3>
+                </Link>
             </Grid>
         </Grid>
     );
