6b1b6dee1530064dc4ecd164c5aac59688f49623
mingddo <godhsb@naver.com>
2025-01-16 16:29:58 +0900

    Update : Main Page Header 로그인 버튼 구역 추가

--- src/app/_component/PageHeader.js
+++ src/app/_component/PageHeader.js
@@ -2,10 +2,16 @@ import { Grid2 as Grid, Link } from "@mui/material";
 
 export default function PageHeader() {
     return (
-        <Grid container direction={"row"} sx={{height : "100%"}}>
-            <Grid className={"text-white text-3xl text-bold"} size={2} sx={{ minWidth : "130px", display : "flex",  justifyContent : "center", alignItems : "center", marginLeft :"50px" }}>
+        <Grid container direction={"row"} sx={{height : "100%", display : "flex", justifyContent : "space-between", alignItems : "center", margin : "0 50px 0 50px"}}>
+            <Grid className={"text-white text-3xl text-bold"} size={2} sx={{minWidth : "130px", }}>
                 <Link href="/home" className={["text_link_color", "link_no_underline"].join(" ")}>
                     <h1>루팡타임</h1>
+                </Link>
+            </Grid>
+            <Grid className={"text-white text-3xl text-bold"} size={2} sx={{ minWidth : "130px",}}
+            >
+                <Link href="/login" className={["text_link_color", "link_no_underline"].join(" ")}>
+                    <h1>로그인</h1>
                 </Link>
             </Grid>
         </Grid>
