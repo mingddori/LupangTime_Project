497c7cb9c9602e9eb46648f8e8975548b9e0b52d
mingddo <godhsb@naver.com>
2025-07-01 17:44:10 +0900

    Temp : 숫자 야구 게임 방 UI 개발 중

--- src/app/(contents)/game/num_baseball/_component/NBGamePlay.js
+++ src/app/(contents)/game/num_baseball/_component/NBGamePlay.js
@@ -1,34 +1,114 @@
 "use client";
-import { Grid2 as Grid, Stack, Button, Typography } from "@mui/material";
+import { Grid2 as Grid, Button, Typography, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow,} from "@mui/material";
 
-export default function NBGamePlay( { params } ) {
+export default function NBGamePlay( props ) {
     
+    const { roomInfo } = props;
     
     return (
-        <Grid container direction={"column"} sx={{ height: "100%", backgroundColor: "darkgreen", padding: "20px 100px 20px 100px" }}>
+        <Grid container gap={"10px"} direction={"column"}
+            sx={{ height: "100%", backgroundColor: "darkgreen",
+                padding: {
+                    xs :"20px",
+                    xl :"20px 100px 20px 100px"
+                }
+            }}
+        >
+            
+            {/* 방 정보 부분 */}
             <Grid size={12} sx={{ height: "50px", backgroundColor: "pink", justifyContent: "center", alignItems: "center" }}>
-                {/* 나가기 버튼 */}
                 <Grid container direction={"row"} sx={{ height: "100%", justifyContent: "space-between", alignItems: "center" }}>
                     {/* 방 번호 */}
-                    <Grid size={2} sx={{ backgroundColor: "lightblue", justifyContent: "center", alignItems: "center" }}>
+                    <Grid container size={2} sx={{ backgroundColor: "lightblue", justifyContent: "center", alignItems: "center" }}>
                         <Typography variant="h6" sx={{ color: "black" }}>
-                            방 번호 : {params.room_num}
+                            방 번호 : {roomInfo}
                         </Typography>
                     </Grid>
+
+                    {/* 준비 버튼 */}
+                    <Grid size={2} sx={{ backgroundColor: "lightblue", justifyContent: "center", alignItems: "center" }}>
+                        <Button variant="contained" color="primary" sx={{ height: "100%", width: "100%" }}>
+                            준비
+                        </Button>
+                    </Grid>
+
+                    {/* 나가기 버튼 */}
                     <Grid size={2} sx={{ backgroundColor: "lightblue", justifyContent: "center", alignItems: "center" }}>
                         <Button variant="contained" color="primary" sx={{ height: "100%", width: "100%" }} href={`/game/num_baseball/players/rooms`}>
                             나가기
                         </Button>
                     </Grid>
                 </Grid>
-
             </Grid>
 
-            <Grid size={12} sx={{ backgroundColor: "pink", justifyContent: "center", alignItems: "center" }}>
+            {/* 게임 부분 */}
+            <Grid container size={12} gap={"10px"} direction={"column"} alignItems={"center"} justifyContent={"center"} sx={{ flex:"1", backgroundColor: "pink"}}>
+                {/* 상대편 영역*/}
+                <Grid container size={12} sx={{height: "30%", backgroundColor: "lightblue"}}>
+                    <Grid>
+                        dd
+                    </Grid>
+                </Grid>
+
+                {/* 내 영역 */}
+                <Grid container size={12} direction={"column"} sx={{height: "65%", backgroundColor: "lightblue"}}>
+                    
+                    {/* 게임 help */}
+                    <Grid container direction={"row"} size={12} justifyContent={"flex-end"} sx={{height: "30px", background:"skyblue", alignItems: "center"}}>
+                        <Grid size={1} sx={{backgroundColor: "lightcoral", justifyContent: "center", alignItems: "center"}}>
+                            물음표 영역
+                        </Grid>
+                    </Grid>
+                    
+                    {/* 게임 결과 영역 */}
+                    <Grid container size={12} sx={{flex : "1", background:"plum", justifyContent: "center", alignItems: "flex-start"}}>
+                        <TableContainer component={Paper} sx={{flex: "1", width: "100%", maxHeight: "100%"}}>
+                            <Table>
+                                <TableHead>
+                                    <TableRow sx={{ height: "10px" }}>
+                                        <TableCell colSpan={5} sx={{p:0}}>시도</TableCell>
+                                    </TableRow>
+                                    <TableRow>
+                                        <TableCell>시도</TableCell>
+                                        <TableCell>공격 숫자</TableCell>
+                                        <TableCell colSpan={3} sx={{textAlign:"center"}}>결과</TableCell>
+                                    </TableRow>
+                                </TableHead>
+                                <TableBody>
+                                    {/* 여기에 게임 결과 데이터를 매핑하여 표시 */}
+                                    {/* 예시 데이터 */}
+                                    {[1, 2, 3, 4, 5, 6, 7].map((attempt) => (
+                                        <TableRow key={attempt}>
+                                            <TableCell>{attempt}</TableCell>
+                                            <TableCell>1234</TableCell>
+                                            <TableCell>1B</TableCell>
+                                            <TableCell>2S</TableCell>
+                                            <TableCell>Out</TableCell>
+                                        </TableRow>
+                                    ))}
+                                </TableBody>
+                            </Table>
+                        </TableContainer>
+                        
+                    </Grid>
+                    
+                    {/* 채팅 영역 */}
+                    <Grid container size={12} direction={"row"} sx={{height : "50px",  background:"khaki", justifyContent: "center", alignItems: "center"}}>
+                        {/* 입력 영역 */}
+                        <Grid>
+                            입력란
+                        </Grid>
+                        {/* 공격 버튼 */}
+                        <Grid>
+                            공격
+                        </Grid>
+                    </Grid>
+                </Grid>
             </Grid>
         </Grid>
     );
 }
+
 
 // function getResult(answer, guess) {
 //     let strike = 0;
--- src/app/(contents)/game/num_baseball/players/rooms/[room_num]/page.js
+++ src/app/(contents)/game/num_baseball/players/rooms/[room_num]/page.js
@@ -3,8 +3,8 @@ import NBGamePlay from "@/app/(contents)/game/num_baseball/_component/NBGamePlay
 
 export default function NumBaseballRoomPage( { params } ) {
     return(
-        <Grid>
-            <NBGamePlay roomInfo={} />
+        <Grid container direction={"column"} sx={{ height: "100%", backgroundColor: "darkgreen" }}>
+            <NBGamePlay roomInfo={params.room_num} />
         </Grid>
     );
 }
